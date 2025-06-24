ac9729ab132ffbd2dc2b48b4e926c8d5d2c1097b
Soumyajit Basu <soumybasu@gmail.com>
2025-06-24 12:25:20 +0530

    code for orgini shift is added

--- cypress/e2e/originChange.cy.ts
+++ cypress/e2e/originChange.cy.ts
@@ -1,16 +1,16 @@
 describe('this a test scenario for website origin change', () => {
 
     it('Login to social platform', () => {
-        cy.visit('https://staging.social.stockedge.com/')
-        cy.get('[data-testid="login-button"]', { timeout: 8000 }).click()
+        // cy.visit('https://staging.social.stockedge.com/')
+        // cy.get('[data-testid="login-button"]', { timeout: 8000 }).click()
 
-        cy.origin('https://staging1-accounts.stockedge.com/', () => {
-            cy.get('#Username').type('kufyesolmu@gufum.com')
-            cy.get('#Password').type('Soumyajit@2022')
-            cy.get('#login-btn').click()
-            cy.wait(6000)
+        // cy.origin('https://staging1-accounts.stockedge.com/', () => {
+        //     cy.get('#Username').type('kufyesolmu@gufum.com')
+        //     cy.get('#Password').type('Soumyajit@2022')
+        //     cy.get('#login-btn').click()
+        //     cy.wait(6000)
 
-        })
+        // })
         //cy.contains('KYC Verification', {timeout: 10000}).should('exist')
     })
 })
