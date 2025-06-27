bc4391efcc2f01ff20a24c7cefddb85ca96932ea
Soumyajit Basu <soumybasu@gmail.com>
2025-06-27 10:56:06 +0530

    session management

--- cypress/e2e/seSocialFeedVerification.cy.ts
+++ cypress/e2e/seSocialFeedVerification.cy.ts
@@ -3,7 +3,7 @@ import { LoginPage } from "../support/page_objects/loginPageObject"
 describe('This is a test for opening SE Social and verifying the Home Feed post', () => {
 
     beforeEach('open SESocial staging environment', () => {
-
+Cypress.session.clearAllSavedSessions()
         cy.visit('https://staging.social.stockedge.com',{timeout:10000})
 
         cy.fixture('../fixtures/userLogin.json').as('userLoginData',)
@@ -11,7 +11,7 @@ describe('This is a test for opening SE Social and verifying the Home Feed post'
     })
 
     it('Login to SESocial', () => {
-                cy.get('[data-testid="login-button"]').click()
+
         cy.get('@userLoginData').then((userLoginData: any) => {
             const login = new LoginPage()
             login.loginWithSE(userLoginData.username, userLoginData.password)
