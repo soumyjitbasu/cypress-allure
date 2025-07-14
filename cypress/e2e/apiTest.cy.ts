b00516e3b61db858912966d5c433719769f2e897
Soumyajit Basu <soumybasu@gmail.com>
2025-07-14 09:26:04 +0530

    added api test scenarios

---
+++ cypress/e2e/apiTest.cy.ts
@@ -0,0 +1,58 @@
+describe('API Testing Scenarios', () => {
+
+    it('Authorization', () => {
+        cy.request({
+            url: 'https://restful-booker.herokuapp.com/auth',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "username": "admin",
+                "password": "password123"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            Cypress.env('token', response.body.token)
+            console.log(Cypress.env('token'))
+        })
+    })
+
+    it('Get Booking', () => {
+        cy.request({
+            url: 'https://restful-booker.herokuapp.com/booking',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        }).then((response) => {
+            console.log(response.body)
+        })
+    })
+
+    it('Create Booking', () => {
+        cy.request({
+            url: 'https://restful-booker.herokuapp.com/booking',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: {
+                "firstname": "Soumyajit",
+                "lastname": "Basu",
+                "totalprice": 111,
+                "depositpaid": true,
+                "bookingdates": {
+                    "checkin": "2025-10-01",
+                    "checkout": "2025-10-02"
+                },
+                "additionalneeds": "Breakfast"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            console.log(response.body)
+        })
+    })
+})
